d0d7df7f44fa0347ef940b97abb8f012a67071c3
balewpewpew <supapitchaitan.2006@gmail.com>
2025-12-30 15:26:18 +0700

    lab12_2.cpp completed!!

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int,int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int a,int b){
+    if(a%b==0){
+        return b;
+    }else{
+        int c = gcd(b,a%b);
+        return c;
+    }
 }
