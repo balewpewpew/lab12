d0923ff390c4c3e190e7a0a619c87cfbe91aed74
balewpewpew <supapitchaitan.2006@gmail.com>
2025-12-30 15:09:16 +0700

    lab12_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int i){
+    if(i==0){
+        return 0;
+    }else if(i==1){
+        return 1;
+    }else{
+        int n = i = fibonacci(i-1)+fibonacci(i-2);
+        return n;
+    }
 }
